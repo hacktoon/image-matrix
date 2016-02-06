9eda7a37877bdd7f940417a1631d71227a8308ad
Karlisson Bezerra <contact@hacktoon.com>
2016-02-06 13:01:17 -0200

    Create support functions for input data

--- editor_grafico.py
+++ editor_grafico.py
@@ -1,9 +1,47 @@
 # !/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
 
-def main():
-    pass
+
+def parse_command(expr):
+    '''
+    Expect a command expression (str)
+        Example: "A 4 6"
+    Return a tuple with the command name and the params (tuple)
+        Example: ('A', [4, 6])
+    '''
+    cmd_alias, params = expr.split(maxsplit=1)
+    return cmd_alias, params.split()
+
+
+def read_input(filename):
+    '''
+    Expect a string filename (str)
+    Return the sanitized file contents in lines (list)
+    '''
+    with open(filename) as f:
+        return [x.strip() for x in f.readlines() if x.strip()]
+
+
+def main(command_list):
+    '''
+    Expect a list of input commands (list)
+    '''
+    for expr in command_list:
+        cmd_alias, params = parse_command(expr)
+        print(cmd_alias, params)
+
 
 if __name__ == '__main__':
-    main()
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        sys.exit('Expected a input file.')
+
+    try:
+        command_lines = read_input(filename)
+    except IOError:
+        sys.exit('File {!r} not found.'.format(filename))
+
+    main(command_lines)
--- tests.py
+++ tests.py
@@ -1,7 +1,35 @@
 # encoding: utf-8
 
 import unittest
+import tempfile
 import editor_grafico as editor
+
+
+class InputTest(unittest.TestCase):
+    def test_input_reader(self):
+        test_data = '''
+        I 4 5
+        C
+        S one.bmp
+        G 2 3 J
+        V 2 3 4 W
+        '''
+        fp = tempfile.NamedTemporaryFile(mode='w+t')
+        fp.write(test_data)
+        fp.seek(0)
+        file_content = editor.read_input(fp.name)
+        assert file_content == [
+            'I 4 5',
+            'C',
+            'S one.bmp',
+            'G 2 3 J',
+            'V 2 3 4 W'
+        ]
+        fp.close()
+
+    def test_error_on_input_reader(self):
+        with self.assertRaises(IOError):
+            editor.read_input('nowhere_file.txt')
 
 
 if __name__ == '__main__':
